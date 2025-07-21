7656554c2d3e415894a0216d7f6d41b59ccbf9b9
hongfeizhou <hongfeizhou@126.com>
2025-07-21 16:23:17 +0800

    ADD image

--- merge_pdf/__manifest__.py
+++ merge_pdf/__manifest__.py
@@ -5,6 +5,7 @@
     'version': '16.0.1.0.0',
     'depends': ['base'],
     'author': 'Bruce Zhou', 
+    "images": ['static/description/view.png'],
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
