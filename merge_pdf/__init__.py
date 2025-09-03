88eb2fd2ccca503c336762e19fd42173779278f2
hongfeizhou <hongfeizhou@126.com>
2025-09-03 11:24:04 +0800

    ADD note

--- merge_pdf/__init__.py
+++ merge_pdf/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
-
+# -----------------------------------------------------------------------------
+# Author: hongfeizhou@126.com
+# Experienced Odoo developer with many years of expertise.
+# Skilled in Python, HTML, and JavaScript for building and customizing solutions.
+# -----------------------------------------------------------------------------
 from . import models
 from . import controllers
